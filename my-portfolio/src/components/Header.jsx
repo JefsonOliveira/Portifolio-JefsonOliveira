aa15e03d90b9a806f701cf52cd43facda8f22aa9
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-06-13 16:01:26 -0300

    feat: adição dos icons no header, iniciando estilização

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,11 +1,22 @@
-import "./Header.css"
+import { FaLinkedin, FaGithubSquare, FaFileAlt } from "react-icons/fa";
+
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div className="header-container">
-            <h1>Header</h1>
+  return (
+    <div className="header-container">
+      <div className="react-with-icons">
+        <h3 className="name-text">OI EU SOU JEFSON</h3>
+        <h1 className="react-text">REACT</h1>
+        <div className="icons">
+          <FaLinkedin />
+          <FaGithubSquare />
+          <FaFileAlt />
         </div>
-    )
-}
+      </div>
+      <h1 className="developer-text">DEVELOPER</h1>
+    </div>
+  );
+};
 
 export default Header;
