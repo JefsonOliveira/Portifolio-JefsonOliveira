78a576850d9fd99609c31468311ef8aca7c01612
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-06-13 23:31:54 -0300

    feat: Estilização do componente Header

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,20 +1,35 @@
-import { FaLinkedin, FaGithubSquare, FaFileAlt } from "react-icons/fa";
-
+import {
+  FaLinkedin,
+  FaGithubSquare,
+  FaFileAlt,
+  FaHandSparkles,
+} from "react-icons/fa";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <div className="react-with-icons">
-        <h3 className="name-text">OI EU SOU JEFSON</h3>
-        <h1 className="react-text">REACT</h1>
-        <div className="icons">
-          <FaLinkedin />
-          <FaGithubSquare />
-          <FaFileAlt />
+    <div className="header-wrapper">
+      <div className="header-container">
+        <div className="react-with-icons">
+          <div className="name-text">
+            OI <FaHandSparkles className="wave-icon" /> EU SOU JEFSON
+          </div>
+          <div className="react-text">
+            REACT
+            <div className="icons">
+              <FaLinkedin />
+              <FaGithubSquare />
+              <FaFileAlt />
+            </div>
+          </div>
         </div>
+        <h1 className="developer-text">DEVELOPER</h1>
+        <p>
+          TENHO 24 ANOS, SOU DESENVOLVEDOR FRONTEND COM 1 ANO DE EXPERIÊNCIA
+          TRABALHANDO COMO FREELANCER.
+        </p>
       </div>
-      <h1 className="developer-text">DEVELOPER</h1>
+      <div className="pixel-transition"></div>
     </div>
   );
 };
